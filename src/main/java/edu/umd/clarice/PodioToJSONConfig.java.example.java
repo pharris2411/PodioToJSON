00b7e1036b7cab09103910d2a1b879fa95b43f77
Peter Harris <peter.harris@constellation.com>
2018-05-03 18:52:49 -0400

    Added example config file

---
+++ src/main/java/edu/umd/clarice/PodioToJSONConfig.java.example.java
@@ -0,0 +1,25 @@
+package edu.umd.clarice;
+
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.TimeZone;
+import java.nio.charset.StandardCharsets;
+
+public final class PodioToJSONConfig {
+	// create an API key on the Podio site
+	public final String apiUsername = "podiotojson";
+	public final String apiKey = "";
+   	
+   	// backup(?) login with an email address and password
+	public final String loginEmail = "";
+	public final String loginPass = "";
+}
+
